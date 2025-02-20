2429dc4963bb2e554aa5dde48503e3b39dbc02b1
gunha0405 <dhrjsgk0405@gmail.com>
2025-02-20 16:17:16 +0900

    [Feat] 로깅 기능(파일) 추가, 데이터 소스 라우팅 설정 추가

--- package404/src/main/java/com/example/package404/board/controller/BoardController.java
+++ package404/src/main/java/com/example/package404/board/controller/BoardController.java
@@ -6,6 +6,8 @@ import com.example.package404.board.model.Dto.BoardRequestDto;
 import com.example.package404.board.model.Dto.BoardResponseDto;
 import com.example.package404.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
