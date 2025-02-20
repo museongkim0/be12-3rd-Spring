8bccdcbeefc293a6a760a7f828e0fe19da9a576a
leewoojin <zxsa999o@gmail.com>
2025-02-20 10:54:48 +0900

    [feat] 강사 수정 및 추가

--- package404/src/main/java/com/example/package404/instructor/model/dto/req/CourseRegister.java
+++ package404/src/main/java/com/example/package404/instructor/model/dto/req/CourseRegister.java
@@ -1,26 +1,34 @@
-package com.example.package404.instructor.model.dto;
+package com.example.package404.instructor.model.dto.req;
 
 import com.example.package404.instructor.model.Course;
+import com.example.package404.instructor.model.Instructor;
 import com.example.package404.user.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
-
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CourseRegister {
 
     private String name;
     private int generation;
+
+
     List<CurriculumRegisterDto> curriculumList = new ArrayList<>();
 
-    public Course toEntity(User user) {
+    public Course toEntity(Instructor instructor) {
         return Course.builder()
                 .name(name)
                 .generation(generation)
-//                .user(user)
+                .instructor(instructor)
                 .build();
+
     }
+
 }
---
+++ package404/src/main/java/com/example/package404/instructor/model/dto/req/InstructorRequestDto.java
@@ -0,0 +1,42 @@
+package com.example.package404.instructor.model.dto.req;
+
+
+import com.example.package404.instructor.model.Instructor;
+import com.example.package404.user.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class InstructorRequestDto {
+
+
+    private String record;
+
+    private String portfolio;
+
+
+    public Instructor toEntity(User user) {
+
+        return Instructor.builder()
+                .record(record)
+                .portfolio(portfolio)
+                .user(user)
+                .build();
+
+
+
+    }
+
+
+
+
+
+
+}
+
