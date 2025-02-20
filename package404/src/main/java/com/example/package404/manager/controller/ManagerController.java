80197aec2e055b6cd5ba379c3ee7b20d9b76c5d0
goodleaf96 <ky1234tho@gmail.com>
2025-02-20 16:21:07 +0900

    시험 dto 및 관련 서비스 추가

--- package404/src/main/java/com/example/package404/manager/controller/ManagerController.java
+++ package404/src/main/java/com/example/package404/manager/controller/ManagerController.java
@@ -1,8 +1,8 @@
 package com.example.package404.manager.controller;
 
 import com.example.package404.instructor.model.dto.InstructorResponseDto;
-import com.example.package404.manager.model.dto.ManagerRequestDto;
 import com.example.package404.manager.model.dto.ManagerResponseDto;
+import com.example.package404.manager.model.dto.TestRequestDto;
 import com.example.package404.manager.service.ManagerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -23,15 +23,30 @@ public class ManagerController {
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/find/manager/{managerIdx}")
+    @GetMapping("/{managerIdx}")
     public ResponseEntity<ManagerResponseDto> findManager(@PathVariable Long managerIdx) {
         ManagerResponseDto response = managerService.getManager(managerIdx);
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/find/instructor/{instructorIdx}")
+    @GetMapping("/instructor/{instructorIdx}")
     public ResponseEntity<InstructorResponseDto> findInstructor(@PathVariable Long instructorIdx) {
         InstructorResponseDto response = managerService.getInstructor(instructorIdx);
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/test/register")
+    public void registerTest(@RequestBody TestRequestDto dto) {
+        managerService.registerTest(dto);
+    }
+
+    @PostMapping("/test/update/{testIdx}")
+    public void updateTest(Long testIdx, @RequestBody TestRequestDto dto) {
+        managerService.updateTest(testIdx, dto);
+    }
+
+    @PostMapping("/test/delete/{testIdx}")
+    public void deleteTest(Long testIdx) {
+        managerService.deleteTest(testIdx);
     }
 }
