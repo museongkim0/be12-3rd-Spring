8a396f8de1dab5843f2c417158ee0a1baeb9e6d7
museongkim <mstar0228@gmail.com>
2025-02-25 18:04:04 +0900

    [Feat] Student Controller, Service -> BaseResponse 응답 적용 완료

--- package404/src/main/java/com/example/package404/student/repository/StudentRepository.java
+++ package404/src/main/java/com/example/package404/student/repository/StudentRepository.java
@@ -1,18 +1,16 @@
 package com.example.package404.student.repository;
 
 import com.example.package404.student.model.StudentDetail;
-import com.example.package404.user.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<StudentDetail, Long> {
 
     @Query("SELECT sd FROM StudentDetail sd JOIN FETCH sd.user u")
-    List<StudentDetail> findAllStudents();
+    Page<StudentDetail> findAllStudents(PageRequest of);
 
     @Query("SELECT sd FROM StudentDetail sd JOIN FETCH sd.user u WHERE u.idx = :idx")
     Optional<StudentDetail> findByStudent(Long idx);
--- package404/src/main/java/com/example/package404/student/service/StudentService.java
+++ package404/src/main/java/com/example/package404/student/service/StudentService.java
@@ -1,18 +1,17 @@
 package com.example.package404.student.service;
 
-import com.example.package404.student.model.Dto.StudentDetailRegisterDto;
-import com.example.package404.student.model.Dto.StudentDetailUpdateDto;
-import com.example.package404.student.model.Dto.StudentResponseDto;
+import com.example.package404.global.exception.StudentException;
+import com.example.package404.global.response.responseStatus.StudentResponseStatus;
+import com.example.package404.student.model.Dto.*;
 import com.example.package404.student.model.StudentDetail;
 import com.example.package404.student.repository.StudentRepository;
 import com.example.package404.user.model.User;
 import com.example.package404.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -20,44 +19,79 @@ public class StudentService {
     private final StudentRepository studentRepository;
     private final UserRepository userRepository;
 
-    public void register(StudentDetailRegisterDto dto, User user){
-        studentRepository.save(dto.toEntity(user));
-    }
+    public StudentDetailResponseDto register(StudentDetailRegisterDto dto, User user) {
+        if (dto == null) {
+            throw new StudentException(StudentResponseStatus.STUDENT_NOT_FOUND);
+        }
 
-    public List<StudentResponseDto> list() {
-//        List<User> studentList = userRepository.findByRoleEquals("ROLE_STUDENT");
-        List<StudentDetail> studentList = studentRepository.findAllStudents();
-        return studentList.stream().map(StudentResponseDto::from).collect(Collectors.toList());
+        try {
+            StudentDetail studentDetail = studentRepository.save(dto.toEntity(user));
+            return StudentDetailResponseDto.from(studentDetail);
+        } catch (Exception e) {
+            throw new StudentException(StudentResponseStatus.STUDENT_ENROLLMENT_FAILED);
+        }
     }
 
     public StudentResponseDto read(Long idx) {
+        if (idx == null || idx < 0 || idx >= studentRepository.count()) {
+            throw new StudentException(StudentResponseStatus.INVALID_STUDENT_ID);
+        }
+
         StudentDetail student = studentRepository.findByStudent(idx).orElseThrow();
+        if (student == null) {
+            throw new StudentException(StudentResponseStatus.STUDENT_NOT_FOUND);
+        }
 
         return StudentResponseDto.from(student);
     }
 
-    public void update(User user, String action) {
+    public StudentDetailPageResponse list(int page, int size) {
+        if (page < 0 || page+1 > studentRepository.count() || size < 0 || size > studentRepository.count()) {
+            throw new StudentException(StudentResponseStatus.INVALID_PAGE);
+        }
+        Page<StudentDetail> studentList = studentRepository.findAllStudents(PageRequest.of(page, size));
+        if (studentList.isEmpty()) {
+            throw new StudentException(StudentResponseStatus.STUDENT_NOT_FOUND);
+        }
+        return StudentDetailPageResponse.from(studentList);
+    }
+
+    public StudentDetailResponseDto update(User user, String action) {
+        if (user.getIdx() == null || user.getIdx() < 0 || user.getIdx() > studentRepository.count()) {
+            throw new StudentException(StudentResponseStatus.INVALID_STUDENT_ID);
+        }
         User findUser = userRepository.findById(user.getIdx()).orElseThrow();
+        if (findUser.getStudentDetail().getIdx() == null || findUser.getStudentDetail().getIdx() < 0) {
+            throw new StudentException(StudentResponseStatus.INVALID_STUDENT_ID);
+        }
         StudentDetail studentDetail = studentRepository.findById(findUser.getStudentDetail().getIdx()).orElseThrow();
 
         if (action.equals("testStatus")) {
             studentDetail.updateTestStatus();
-            studentRepository.save(studentDetail);
+            StudentDetail studentDetailR = studentRepository.save(studentDetail);
+            return StudentDetailResponseDto.from(studentDetailR);
         } else if (action.equals("perception")) {
             studentDetail.updatePerception();
-            studentRepository.save(studentDetail);
+            StudentDetail studentDetailR = studentRepository.save(studentDetail);
+            return StudentDetailResponseDto.from(studentDetailR);
         } else if (action.equals("attendance")) {
             studentDetail.updateAttendance();
-            studentRepository.save(studentDetail);
+            StudentDetail studentDetailR = studentRepository.save(studentDetail);
+            return StudentDetailResponseDto.from(studentDetailR);
         } else if (action.equals("leaveEarly")) {
             studentDetail.updateLeaveEarly();
-            studentRepository.save(studentDetail);
+            StudentDetail studentDetailR = studentRepository.save(studentDetail);
+            return StudentDetailResponseDto.from(studentDetailR);
         } else if (action.equals("outing")) {
             studentDetail.updateOuting();
-            studentRepository.save(studentDetail);
+            StudentDetail studentDetailR = studentRepository.save(studentDetail);
+            return StudentDetailResponseDto.from(studentDetailR);
         } else if (action.equals("vacationLeft")) {
             studentDetail.updateVacationLeft();
-            studentRepository.save(studentDetail);
+            StudentDetail studentDetailR = studentRepository.save(studentDetail);
+            return StudentDetailResponseDto.from(studentDetailR);
+        } else {
+            throw new StudentException(StudentResponseStatus.INVALID_UPDATE_ACTION);
         }
     }
 }
