68655dd441dfefcc82cf546f97b26b4b5031099d
leewoojin <zxsa999o@gmail.com>
2025-02-27 17:59:25 +0900

    [feat] baseresponse조금 수정 및 강사 list 조회

---
+++ package404/src/main/java/com/example/package404/instructor/TodoList.java
@@ -0,0 +1,14 @@
+package com.example.package404.instructor;
+
+public class TodoList {
+
+    //Todo
+    // 강사의 강의 onetoone 에서 -> oneToMany 로 고쳐야함
+
+    //Todo
+    // 학생이 강사의 정보 조회를 어디까지 가능하게 할건가?
+
+    //Todo
+    //강사가 자기 정보 수정하는데 user 랑 instructor 정보 수정을 같이 할지 따로 할지
+
+}
